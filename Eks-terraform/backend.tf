6b5813446add6b3a3d05651d43713a9df3c5e642
maheshdte <maheshdte@gmail.com>
2024-02-20 15:48:17 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "maheshdte-jenkins-020224" # Replace with your actual S3 bucket name
+    bucket = "maheshdte-jenkins-020225" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "us-east-1"
   }
